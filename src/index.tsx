6b0a67183a65fd41ac1aa83afcff16890098e6ed
Kayky Freitas <kayky_defreitas@hotmail.com>
2022-05-07 01:59:54 -0300

    Listando transações e formatando valores

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 import { App } from './App';
 
 createServer({
+  models: {
+    transaction: Model
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de website',
+          type: 'deposit',
+          category: 'dev',
+          amount: 6800,
+          createdAt: new Date('2021-02-12 09:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Compras do mercado',
+          type: 'withdraw',
+          category: 'casa',
+          amount: 500,
+          createdAt: new Date('2021-02-11 19:00:00'),
+        },
+        {
+          id: 3,
+          title: 'Salario',
+          type: 'deposit',
+          category: 'dev',
+          amount: 15000,
+          createdAt: new Date('2021-02-5 17:00:00'),
+        },
+      ]
+    })
+  },
+
   routes() {
     this.namespace = 'api';
 
     this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Transaction 1',
-          amount: 400,
-          type: 'deposit',
-          category: 'Food',
-          createdAt: new Date()
-        }
+      return this.schema.all('transaction')
+    });
 
-      ]
+    this.post('/transactions', (schema, request) =>{
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create('transaction', data)
     })
   }
 })
