a80a50b512800b0bb636a3c2a00f35bc4369006e
Kayky Freitas <kayky_defreitas@hotmail.com>
2022-05-07 00:40:41 -0300

    Finalizando visual da modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
+import { useState } from "react";
+import Modal from "react-modal";
+import { NewTransactionModal } from './components/NewTransactionModal';
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyles } from "./styles/global";
 
+Modal.setAppElement('#root');
+
 export function App() {
+
+  const [isNewTransactionModalOpens, setIsNewTransactionModalOpens] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+      setIsNewTransactionModalOpens(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+      setIsNewTransactionModalOpens(false);
+  }
+
   return (
     <>
       <GlobalStyles />
-      <Header />
+      <Header onOpenNewTransactionModal={ handleOpenNewTransactionModal } />
       <Dashboard />
+      <NewTransactionModal 
+        isOpen={ isNewTransactionModalOpens }
+        onRequestClose={ handleCloseNewTransactionModal }
+      />
     </>
   );
 }
